c57eb6e91da2bc7abb6040451c7f6635b786c800
Bryan <bryanrr_julio_@hotmail.com>
2021-10-22 14:32:06 -0500

    Initial gateway config

---
+++ autoservicio-gateway/src/main/java/com/autoservicio/autoserviciogateway/AutoservicioGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.autoservicio.autoserviciogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoservicioGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoservicioGatewayApplication.class, args);
+	}
+
+}
